c32159abbb4938432ed9111fd367a25ca14272fb
Ying Mei Lum <ymeiosis@gmail.com>
2018-02-19 17:39:47 +0800

    fusuma habis

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 import Fusuma
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, FusumaDelegate {
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        print("Image selected")
+    }
+    
+    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
+        print("Multiple images selected")
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        print("Called just after a video has been selected.")
+    }
+    
+    func fusumaCameraRollUnauthorized() {
+        print("Camera roll unauthorized")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Fusuma()
         
         // Do any additional setup after loading the view.
     }
@@ -22,14 +39,16 @@ class CameraViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-//    func Fusuma() {
-//        let fusuma = FusumaViewController()
-//        fusuma.delegate = self
-//        fusuma.availableModes = [.library, .camera, .video] // The default value is [.library, .camera].
-//        fusuma.cropHeightRatio = 0.6 // Height-to-width ratio. The default value is 1, which means a squared-size photo.
-//        fusuma.allowMultipleSelection = true // You can select multiple photos from the camera roll. The default value is false.
-//        self.presentViewController(fusuma, animated: true, completion: nil)
-//    }
+    func Fusuma() {
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.availableModes = [.library, .camera, .video] // The default value is [.library, .camera].
+        fusuma.cropHeightRatio = 0.6 // Height-to-width ratio. The default value is 1, which means a squared-size photo.
+        fusuma.allowMultipleSelection = true // You can select multiple photos from the camera roll. The default value is false.
+        
+        
+        self.present(fusuma, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation

8a421e41c548cbd8cc19cd41ac5e462d07646e5e
Philip Teow <ptxy.1897@gmail.com>
2018-02-19 15:09:13 +0800

    Set up feedVC and commentVC

---
+++ Instagram/CommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentTableViewCell.swift
+//  Instagram
+//
+//  Created by Philip Teow on 19/02/2018.
+//  Copyright © 2018 Ying Mei Lum. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

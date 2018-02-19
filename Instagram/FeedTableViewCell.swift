d229589390e1bcfadb3df28efb79fd59b6b4f435
Philip Teow <ptxy.1897@gmail.com>
2018-02-19 15:10:32 +0800

    set up feedVC and commentVC

--- Instagram/FeedTableViewCell.swift
+++ Instagram/FeedTableViewCell.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class FeedTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postedImageView: UIImageView!
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var commentImageView: UIImageView!
+    @IBOutlet weak var numOfLikesLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
+    
+    
+    
+    
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)

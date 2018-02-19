d641df2604ddc5158c22c4e838ad4a52dbe7f164
Ying Mei Lum <ymeiosis@gmail.com>
2018-02-19 18:19:50 +0800

    removed fusuma video, pending handle

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -43,13 +43,13 @@ class CameraViewController: UIViewController, FusumaDelegate {
     func Fusuma() {
         let fusuma = FusumaViewController()
         fusuma.delegate = self
-        fusuma.availableModes = [.library, .camera, .video] // The default value is [.library, .camera].
+        fusuma.availableModes = [.library, .camera] // The default value is [.library, .camera].
         fusuma.cropHeightRatio = 0.6 // Height-to-width ratio. The default value is 1, which means a squared-size photo.
         fusuma.allowMultipleSelection = true // You can select multiple photos from the camera roll. The default value is false.
         
         
-        self.present(fusuma, animated: true, completion: nil)
-        
+//        self.present(fusuma, animated: true, completion: nil)
+        self.parent?.present(fusuma, animated: true, completion: nil)
     }
 
     /*

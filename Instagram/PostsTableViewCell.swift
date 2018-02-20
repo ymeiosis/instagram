301a4161755a65e110e37d7321f603d6a0cf6fb0
Philip Teow <ptxy.1897@gmail.com>
2018-02-20 12:50:41 +0800

    instantiate commentVC

---
+++ Instagram/PostsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostsTableViewCell.swift
+//  Instagram
+//
+//  Created by Philip Teow on 20/02/2018.
+//  Copyright © 2018 Ying Mei Lum. All rights reserved.
+//
+
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

e32ac6726ee7b772818dee23dbdc2f86e833d5cd
Philip Teow <ptxy.1897@gmail.com>
2018-02-20 12:51:18 +0800

    instantiate commentVC

--- Instagram/CommentViewController.swift
+++ Instagram/CommentViewController.swift
@@ -10,26 +10,29 @@ import UIKit
 
 class CommentViewController: UIViewController {
 
+    
+    
+    var selectedPost : Post?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
+
+extension CommentViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+
--- Instagram/PostsTableViewCell.swift
+++ Instagram/PostsTableViewCell.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class PostsTableViewCell: UITableViewCell {
+protocol PostsTableViewCellDelegate {
+    func goToCommentViewController(at : IndexPath)
+}
 
+class PostsTableViewCell: UITableViewCell {
+    @IBOutlet weak var profilePicImageView: UIImageView! {
+        didSet {
+            profilePicImageView.layer.borderWidth = 1.0
+            profilePicImageView.layer.masksToBounds = false
+            profilePicImageView.layer.borderColor = UIColor.white.cgColor
+            profilePicImageView.layer.cornerRadius = profilePicImageView.frame.size.width / 2
+            profilePicImageView.clipsToBounds = true
+        }
+    }
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postedImageView: UIImageView!
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var commentImageView: UIImageView! {
+        didSet {
+            commentImageView.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(commentImageViewTapped))
+            commentImageView.addGestureRecognizer(tap)
+        }
+    }
+    @IBOutlet weak var usernameLabel2: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var numOfLikesLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    var delegate : PostsTableViewCellDelegate?
+    var atIndexPath : IndexPath = IndexPath()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    @objc func commentImageViewTapped (){
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        delegate?.goToCommentViewController(at: atIndexPath)
+        
+        
     }
+    
+
+
     
 }
